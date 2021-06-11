1c7163010c0157d96105b4339d7905cf72648dfc
Mershelle Rivera <32172992+himynameismoose@users.noreply.github.com>
2021-06-11 22:35:30 +0000

    Completed lab

---
+++ main.py
@@ -0,0 +1,73 @@
+# Lab 1.3: Printing & Variables
+
+# Part 1: Printing Practice
+# There are five lines of code provided to you in the Google Document.
+# Enter the five lines of Python code below, and run it to see what happens.
+# Make sure to respond to the prompts in the Google Document.
+# /ghttps://docs.google.com/document/d/1Wt4zeX6yIoUBUqEyevYytRpU4yjKUwGLn4bxDqt8YPg/edit?usp=sharing
+
+print("Part 1")
+print()
+
+print("1")
+# Expected output: 1
+print(1)
+# Expected output: 1
+print(1 + 2)
+# Expected output: 3
+print("1" + "2")
+# Expected output: 12
+print("this" + " " + "is" + " " + "a" + " " + "sentence" + ".")
+# Expected output: this is a sentence.
+
+print("-----------------------------------------------")
+
+# Part 2: Variables Practice
+# There are two lines of code provided to you in the Google Document.
+# Enter the two lines of Python code below, and run it to see what happens.
+# Make sure to respond to the prompts in the Google Document.
+
+print("Part 2")
+print()
+
+animal = "dogs"
+print(animal + "are really cool.")
+
+print("-----------------------------------------------")
+
+# What happens?
+# The output is: dogsare really cool.
+
+# How would you make the program print out "cats are really cool" instead?
+# Reassign the variable 'animal' to string 'cats' and add a space before the word 'are' in the print statement.
+
+# Part 3: From Snap to Python
+# There is an image of some Snap code provided to you in the Google Document.
+# Re-write the Snap code in Python below. Run it to make sure it works.
+
+print("Part 3")
+print()
+
+number = 100
+print(number)
+number_2 = 100 + number
+print(number_2)
+
+print("-----------------------------------------------")
+
+# Part 4: Four Fours
+# Please see the Google Document for instruction.
+# Write your code below. Run it to make sure it works.
+
+print("Part 4")
+print()
+
+print("Zero is", 44 - 44)
+print("One is", 44 / 44)
+print("Two is", 4 * 4 / (4 + 4))
+print("Three is", (4 + 4 + 4) / 3)
+print("Four is", (4 - 4) / 4 + 4 )
+print()
+print("Extra Credit:")
+print("Five is", ((4 * 4) + 4) / 4)
+print("Six is", (4 + 4) / 4 + 4)
